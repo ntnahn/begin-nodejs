1103deb52472ea1d574374b90ced44bb1ea2188c
THANHNHAN <ntnahn@gmail.com>
2017-01-02 22:34:23 +0700

    add somethings

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,40 +1,24 @@
-jQuery(document).ready(function($) {    
-	//selector đến menu cần làm việc
-	var TopFixMenu = $("#fixNav");
-	// dùng sự kiện cuộn chuột để bắt thông tin đã cuộn được chiều dài là bao nhiêu.
+$(function() {
+	// Selector đến menu cần làm việc
+	var topFixMenu = $("#fixNav");
+	// Dùng sự kiện cuộn chuột để bắt thông tin đã cuộn được chiều dài là bao nhiêu.
 	var scrollTopOld = 0;
-	TopFixMenu.show();
-    $(window).scroll(function(){
-    // Nếu scrollUp thì show Menu
-	if(scrollTopOld > $(this).scrollTop()){//scroll up
-		TopFixMenu.show();
-	}
-	else{
-		TopFixMenu.hide();
-	}
-	scrollTopOld = $(this).scrollTop();
+	topFixMenu.show();
+    $(window).scroll(function() {
+        // Nếu scrollUp thì show Menu
+    	if(scrollTopOld > $(this).scrollTop()) { // Scroll up
+    		topFixMenu.show();
+    	} else {
+    		topFixMenu.hide();
+    	}
+    	scrollTopOld = $(this).scrollTop();
 		// Nếu cuộn được hơn 150px rồi
         /*if($(this).scrollTop()>150){
         // Tiến hành show menu ra    
-        TopFixMenu.show();
+        topFixMenu.show();
         }else{
         // Ngược lại, nhỏ hơn 150px thì hide menu đi.
-            TopFixMenu.hide();
+            topFixMenu.hide();
         }*/
 	});
-	var buttonEvent = $("#event01");
-	buttonEvent.click(function(){
-        alert("Button event are click.");
-    });
-
-    var buttonPlus = $("#button-plus");
-    var buttonMinus = $("#button-minus");
-    var buttonDiv = $("#button-div");
-    var buttonMultiply = $("#button-multiply");
-    var value01 = $("#value01");
-    var value02 = $("#value02");
-    var result = $("#result");
-    buttonMultiply.click(function(){
-        alert("Value is: "+value01.value);
-    });
 });
