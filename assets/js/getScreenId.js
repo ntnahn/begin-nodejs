db29b6cf8b15343c3c8034f5f08f1428a23c846a
Nguyen Thanh Nhan <nhannguyen@vinaecom.com>
2018-03-23 16:39:53 +0700

    Try to install screenshare chrome extension inline

--- assets/js/getScreenId.js
+++ assets/js/getScreenId.js
@@ -117,7 +117,7 @@ getScreenId(function (error, sourceId, screen_constraints) {
 
             loadCallback();
         };
-        iframe.src = 'https://www.webrtc-experiment.com/getSourceId/'; // https://wwww.yourdomain.com/getScreenId.html
+        iframe.src = 'https://tesse.io/getScreenId.html'; // https://wwww.yourdomain.com/getScreenId.html
         iframe.style.display = 'none';
         (document.body || document.documentElement).appendChild(iframe);
     }
